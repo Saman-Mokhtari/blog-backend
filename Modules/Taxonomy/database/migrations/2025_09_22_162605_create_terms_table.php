30d20d6eccd40751a569e3532d24a5abbcf45acb
Saman Mokhtari <saman.mokhtari99@gmail.com>
2025-09-22 20:34:02 +0330

    feat(migrations): Taxonomy migrations created (#8)

---
+++ Modules/Taxonomy/database/migrations/2025_09_22_162605_create_terms_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('terms', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('term');
+            $table->string('slug')->unique();
+            $table->foreignUuid('taxonomy_id')->constrained();
+            $table->foreignUuid('parent_id')->constrained('terms')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('terms');
+    }
+};
